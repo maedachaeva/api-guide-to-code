c5d58f88e056ca65373624c149bb85510c969d3b
maedachaeva <parkchaeyeon.b@gmail.com>
2026-02-12 12:43:19 +0900

    feat: 비즈니스 예외 공통 클래스 추가

---
+++ statistics/src/main/java/com/demo/statistics/global/error/BusinessException.java
@@ -0,0 +1,10 @@
+package com.demo.statistics.global.error;
+
+public class BusinessException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
